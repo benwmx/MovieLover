8bddb3d15dfd978ddad680aea4abd0fc25211448
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-09-15 00:44:32 +0100

    Fix linter errors

--- src/Comment.js
+++ src/Comment.js
@@ -1,23 +1,23 @@
 export default class Comment {
   constructor() {
-    this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/06lGJozlqpSMU5gXuf4o/comments'
+    this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/06lGJozlqpSMU5gXuf4o/comments';
     this.list = null;
   }
 
   async add(id, name, comment) {
-    await fetch(url, {
+    await fetch(this.url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 
+      body: JSON.stringify({
         item_id: id,
         username: name,
-        comment: comment,
+        comment,
       }),
     });
   }
 
   async getList(id) {
-    const response = await fetch(`${url}?item_id=${id}`);
+    const response = await fetch(`${this.url}?item_id=${id}`);
     this.list = await response.json();
   }
-};
+}
