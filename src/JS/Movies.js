5dcf787c489c3b9d620bea7b4c62668a99751a51
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-14 20:02:22 +0430

    create Movies class with a method to request data from API

---
+++ src/JS/Movies.js
@@ -0,0 +1,16 @@
+export default class Movies {
+    constructor(type) {
+        this.baseUrl = 'https://imdb-api.com/en/API/';
+        this.apiKey = 'k_fmzjathd';
+        this.type = type;
+        this.data = null;
+    }
+
+    async getData() {
+        const endpoint = `${this.baseUrl + this.type}/${this.apiKey}`;
+        await fetch(endpoint)
+            .then((response) => response.json())
+            .then((data) => { this.data = data.items;
+            console.log(this.data) });
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/style.css';
+import Movies from './js/Movies.js';
+const comingSoon = new Movies('ComingSoon');
+comingSoon.getData();
+
+
