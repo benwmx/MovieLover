7ebe34198b0930c91a56a0b8d870e62064dbc6c1
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-15 15:28:01 +0430

    work on styling elements

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ const displayList = (data, elementId) => {
     const section = document.getElementById(elementId);
     const list = document.createElement('div');
     list.id = 'list';
-    list.classList.add('d-flex');
-    list.classList.add('justify-content-center');
+    list.classList.add('list');
     if (data !== []) {
         data.forEach((row) => {
             const { id, image, title } = row;
-            const card = `<div id="${id}" class="card" style="width: 18rem;">
+            const card = `<div id="${id}" class="card m-2 bg-dark text-light" style="width: 12rem;">
         <img src="${image}" class="card-img-top" alt="${title}">
         <div class="card-body">
           <h5 class="card-title">${title}</h5>
+          <div class="">
+          <i class="far fa-heart text-danger my-3"></i> 100
+          <i class="fas fa-comment-dots mx-2"></i> 10
+          </div>
           <a href="#" class="btn btn-primary">Add to Watch list</a>
         </div>
       </div>`;
@@ -22,7 +25,22 @@ const displayList = (data, elementId) => {
     section.innerHTML = '';
     section.appendChild(list);
 };
-const comingSoon = new Movies('ComingSoon');
-comingSoon.getData().then(() => {
-    displayList(comingSoon.data, 'comingSoon');
-});
+// const comingSoon = new Movies('ComingSoon');
+// comingSoon.getData().then(() => {
+//     console.log('recived');
+//     displayList(comingSoon.data, 'comingSoon');
+// });
+const tempData = [{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+{id:'abc', image:'https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg', title:'Test Movie'},
+];
+displayList(tempData, 'comingSoon');
