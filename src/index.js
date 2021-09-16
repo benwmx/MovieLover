be631bc5d96d4d00f938ee7ab63032d1bf8a9b27
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-09-16 01:57:31 +0100

    Add eventListener for adding a comment.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './CSS/style.css';
 import './CSS/popup.css';
+import Comment from './JS/Comment.js';
+import { displayComments } from './JS/display.js';
+
+const comment = new Comment();
+const submiComment = document.getElementById('submit');
+const name = document.getElementById('name');
+const commentDescription = document.getElementById('comment-description');
+// this id is a mock-up for the clicked movie.
+const id = '101test';
+
+submiComment.addEventListener('click', (event) => {
+  event.preventDefault();
+  comment.add(id, name.value, commentDescription.value).then(() => {
+    comment.getList(id).then((data) => {
+      displayComments(data);
+    });
+  });
+});
