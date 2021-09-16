5110b502b17a5701f08066eb71b4b2f42d47317d
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-09-16 01:33:31 +0100

    Add displayMovie function

--- src/JS/display.js
+++ src/JS/display.js
@@ -1,6 +1,7 @@
 const displayComments = (comments) => {
+  const list = document.querySelector('.comments-list');
+  list.innerHTML = '';
   comments.forEach((comment) => {
-    const list = document.querySelector('#comments');
     const container = document.createElement('li');
     container.className = 'comment-container';
     const time = document.createElement('p');
@@ -9,10 +10,22 @@ const displayComments = (comments) => {
     name.className = 'comment-name';
     const description = document.createElement('p');
     description.className = 'comment-description';
-
     time.value = comment.creation_date;
     name.value = comment.username;
     description.value = comment.comment;
     list.appendChild(container);
   });
 };
+
+const displayMovieDetails = (movie) => {
+  const title = document.querySelector('.title');
+  const poster = document.querySelector('#poster');
+  const releaseDate = document.querySelector('#date');
+  const votingAverage = document.querySelector('#vote');
+  title.value = movie.title;
+  poster.src = movie.image;
+  releaseDate.value = movie.release_date;
+  votingAverage.value = movie.vote_average;
+};
+
+export { displayComments, displayMovieDetails };
